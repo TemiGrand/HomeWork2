0324860f125b902b87f85c63c9fddf2fd9ce7144
TemiGrand <125796794+TemiGrand@users.noreply.github.com>
2024-06-16 12:32:05 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+copleted_homework = 12
+quantity_hours = 1.5
+cours_name = "Python"
+average_time = quantity_hours/copleted_homework
+print("Курс:", cours_name +
+      ", всего задач:" + str(copleted_homework) +
+      ", затрачено часов:", str(quantity_hours) +
+      ", среднее время выполнения", str(average_time), "часа.")
